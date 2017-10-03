6eee1fbb59f3890cfe22f2bfbbc329b88eff90af
mangukiya-ketan <ketan.mangukiya14@gmail.com>
2017-10-03 16:57:12 +0530

    add columns to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit(:first_name, :last_name, :email, :password,
+        :password_confirmation, :current_password, :mobile)
+    end
+    devise_parameter_sanitizer.permit(:account_update) do |user_params|
+      user_params.permit(:first_name, :last_name, :email, :password,
+        :password_confirmation, :current_password, :mobile)
+    end
+  end
 end
